f32d3bf5e115ffda020cacb4efbe1e82d695dfee
rafiaditya791-droid <rafiaditya791@gmail.com>
2026-01-03 16:01:00 +0000

    rafi

--- tests/Feature/Auth/PasswordUpdateTest.php
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -38,3 +38,5 @@ test('correct password must be provided to update password', function () {
         ->assertSessionHasErrorsIn('updatePassword', 'current_password')
         ->assertRedirect('/profile');
 });
+
+
